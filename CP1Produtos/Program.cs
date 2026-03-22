5d64ad83910c9b359199d50e1d18cd23fd8c03cf
uKarlito <dedepimpu@gmail.com>
2026-03-22 15:20:45 -0300

    Criacao do método para compreender tanto a "," quanto o "." no preço unitário.

--- CP1Produtos/Program.cs
+++ CP1Produtos/Program.cs
@@ -1,34 +1,48 @@
 ﻿
+// Método para aceitar "," e "." como separador decimal
+static double LerDecimal(string mensagem)
+{
+    while (true)
+    {
+        Console.Write(mensagem);
+        string entrada = Console.ReadLine();
+
+        entrada = entrada.Replace(',', '.');
+
+        if (double.TryParse(entrada, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out double valor))
+        {
+            return valor;
+        }
+
+        Console.WriteLine("Valor inválido, tente novamente.");
+    }
+}
 
 // Produto 1
-Console.WriteLine("Produto 1:");
+Console.WriteLine("===== Produto 1 =====");
 Console.Write("Nome: ");
 string nome1 = Console.ReadLine();
 
-Console.Write("Preço Unitário: ");
-double preco1 = double.Parse(Console.ReadLine());
+double preco1 = LerDecimal("Preço Unitário: "); // Usando o "LerDecimal" com double para ajudar o valor com ","
 
 Console.Write("Quantidade: ");
 int quantidade1 = int.Parse(Console.ReadLine());
 
-Console.Write("Desconto: ");
-double desconto1 = double.Parse(Console.ReadLine());
+double desconto1 = LerDecimal("Desconto: "); // Usando o "LerDecimal" com double para ajudar o valor com ","
 
 Produto p1 = new Produto(nome1, preco1, quantidade1, desconto1);
 
 // Produto 2
-Console.WriteLine("Produto 2:");
+Console.WriteLine("\n===== Produto 2 =====");
 Console.Write("Nome: ");
 string nome2 = Console.ReadLine();
 
-Console.Write("Preço Unitário: ");
-double preco2 = double.Parse(Console.ReadLine());
+double preco2 = LerDecimal("Preço Unitário: "); // Usando o "LerDecimal" com double para ajudar o valor com ","
 
 Console.Write("Quantidade: ");
 int quantidade2 = int.Parse(Console.ReadLine());
 
-Console.Write("Desconto: ");
-double desconto2 = double.Parse(Console.ReadLine());
+double desconto2 = LerDecimal("Desconto: "); // Usando o "LerDecimal" com double para ajudar o valor com ","
 
 Produto p2 = new Produto(nome2, preco2, quantidade2, desconto2);
 
@@ -46,5 +60,5 @@ if (p1.CalcularCustoFinal() > p2.CalcularCustoFinal())
 }
 else
 {
-    Console.WriteLine("O produto com maior custo final é: " + p2.GetNome); 
+    Console.WriteLine("O produto com maior custo final é: " + p2.GetNome()); 
 }
--- CP1Produtos/obj/Debug/net10.0/CP1Produtos.AssemblyInfo.cs
+++ CP1Produtos/obj/Debug/net10.0/CP1Produtos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CP1Produtos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db20028c66345d20e936d8c5ee4318f88064560c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d66a9791a7e3ae04c8f5fa624d2a8c5e81ad1625")]
 [assembly: System.Reflection.AssemblyProductAttribute("CP1Produtos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CP1Produtos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
